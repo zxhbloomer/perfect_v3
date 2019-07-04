deb92721a90c1c63c52f21a93b8423bf30af6407
zxhbloomer@gmail.com <Lyyzxh030324>
2019-07-04 23:11:00 +0800

    2019070101

--- perfect-common/src/main/java/com/perfect/common/utils/ExceptionUtil.java
+++ perfect-common/src/main/java/com/perfect/common/utils/ExceptionUtil.java
@@ -1,12 +1,24 @@
 package com.perfect.common.utils;
 
 
+import com.alibaba.fastjson.JSON;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
 
+@Component
 public class ExceptionUtil {
+
+    private static boolean SIMPLE_MODEL;
+
+    @Value("${perfect.config.simple-model}")
+    public void setSIMPLE_MODEL(boolean SIMPLE_MODEL) {
+        this.SIMPLE_MODEL = SIMPLE_MODEL;
+    }
 
     /**
      * 将异常日志转换为字符串
@@ -14,13 +26,19 @@ public class ExceptionUtil {
      * @return
      */
     public static String getException(Exception e) {
+        String rtn = "";
+        if (SIMPLE_MODEL){
+            rtn = e.toString();
+            return rtn;
+        }
         Writer writer = null;
         PrintWriter printWriter = null;
         try {
             writer = new StringWriter();
             printWriter = new PrintWriter(writer);
             e.printStackTrace(printWriter);
-            return writer.toString();
+            rtn = writer.toString();
+            return rtn;
         } finally {
             try {
                 if (writer != null)
