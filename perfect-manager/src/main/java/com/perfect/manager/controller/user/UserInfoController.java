3692da593432ce5146c9d8c80c5613793aa809c3
zxhbloomer@gmail.com <Lyyzxh030324>
2019-07-09 23:46:05 +0800

    2019070101

--- perfect-manager/src/main/java/com/perfect/manager/controller/user/UserInfoController.java
+++ perfect-manager/src/main/java/com/perfect/manager/controller/user/UserInfoController.java
@@ -1,23 +1,16 @@
 package com.perfect.manager.controller.user;
 
-import com.perfect.bean.vo.TokenLoginVo;
+import com.perfect.bean.pojo.JSONResult;
 import com.perfect.bean.vo.user.info.UserInfoVo;
 import com.perfect.common.annotation.SysLog;
 import com.perfect.common.base.controller.v1.BaseController;
 import com.perfect.core.service.client.user.IMUserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/v1/user")
@@ -31,7 +24,8 @@ public class UserInfoController extends BaseController {
     @SysLog("获取用户信息")
     @ApiOperation("获取用户信息")
     @GetMapping("/info")
-    public UserInfoVo userInfo(@RequestParam("token") String token) {
+    @ResponseBody
+    public ResponseEntity<JSONResult<UserInfoVo>> userInfo(@RequestParam("token") String token) {
 
         UserInfoVo rtnBean = imUserService.getUserInfo(token);
 //        ResponseEntity<OAuth2AccessToken
