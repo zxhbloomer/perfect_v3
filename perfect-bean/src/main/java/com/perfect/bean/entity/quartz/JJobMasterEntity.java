7baf0ec1c1e389d6695a74f0f11d64d194ff0519
zxhbloomer@gmail.com <Lyyzxh030324>
2019-07-13 18:36:53 +0800

    2019070101

--- perfect-bean/src/main/java/com/perfect/bean/entity/quartz/JJobMasterEntity.java
+++ perfect-bean/src/main/java/com/perfect/bean/entity/quartz/JJobMasterEntity.java
@@ -1,9 +1,6 @@
 package com.perfect.bean.entity.quartz;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
