416a2dec5d56331366ec1efd3ac0b815c6a9f8ee
zxhbloomer <zxhbloomer@gmail.com>
2019-08-07 15:41:51 +0800

    ..

--- perfect-manager/src/main/java/com/perfect/manager/controller/sys/rabc/RoleController.java
+++ perfect-manager/src/main/java/com/perfect/manager/controller/sys/rabc/RoleController.java
@@ -8,22 +8,37 @@ import com.perfect.bean.vo.sys.rabc.role.SRoleExportVo;
 import com.perfect.bean.vo.sys.rabc.role.SysRoleVo;
 import com.perfect.common.annotation.SysLog;
 import com.perfect.common.base.controller.v1.BaseController;
+import com.perfect.common.constant.PerfectConstant;
 import com.perfect.common.exception.InsertErrorException;
 import com.perfect.common.exception.UpdateErrorException;
+import com.perfect.common.utils.JsonConvertUtil;
 import com.perfect.common.utils.bean.BeanUtilsSupport;
 import com.perfect.core.service.system.rabc.ISRoleService;
+import com.perfect.excel.bean.importconfig.template.ExcelTemplate;
 import com.perfect.excel.export.ExcelUtil;
+import com.perfect.excel.upload.JxlExcelReader;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ooxml.POIXMLDocument;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.poifs.filesystem.FileMagic;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.util.TempFile;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * @author zhangxh
@@ -114,15 +129,32 @@ public class RoleController extends BaseController {
     @ApiOperation("角色数据模板导入")
     @PostMapping("/import")
     public void importData(@RequestBody(required = false) SysRoleVo uploadData,
-        HttpServletResponse response){
+        HttpServletResponse response) throws IOException {
 
         // file bean 保存数据库
 
-        // 文件下载
+        // 文件下载到流
+        String fileUrl = uploadData.getFsType2Url();
+        ResponseEntity<byte[]> rtnResponse = restTemplate.getForEntity(fileUrl, byte[].class);
+        InputStream is =  new ByteArrayInputStream(rtnResponse.getBody());
 
-        // 文件分析
+        // 文件分析，判断是否是excel文档
+        if (FileMagic.valueOf(is) == FileMagic.OLE2){
+            // Office 2003 ，xls
+        } else if (FileMagic.valueOf(is) == FileMagic.OOXML) {
+            // Office 2007 +，xlsx
+        } else {
+            // 非excel文档，报错
+            throw new IllegalArgumentException("导入的文件不是office excel，请选择正确的文件来进行上传");
+        }
 
         // 文件导入
+        // 1、获取模板配置类
+        String json = "{\"dataRow\":{\"dataCols\":[{\"colIndex\":0,\"convertor\":\"datetime\",\"name\":\"type\"},{\"colIndex\":1,\"convertor\":\"date\",\"listValiDatorBean\":[{\"validtorName\":\"required\"},{\"validtorName\":\"datetime\"}],\"name\":\"code\"},{\"colIndex\":2,\"name\":\"name\"},{\"colIndex\":3,\"name\":\"descr\"},{\"colIndex\":4,\"name\":\"simpleName\"}]},\"titleRow\":{\"cols\":[{\"colSpan\":1,\"title\":\"角色类型\"},{\"colSpan\":1,\"title\":\"角色编码\"},{\"colSpan\":1,\"title\":\"角色名称\"},{\"colSpan\":1,\"title\":\"描述\"},{\"colSpan\":1,\"title\":\"简称\"}]}}\n";
+        ExcelTemplate et =  JsonConvertUtil.json2Obj(json, ExcelTemplate.class);
+        JxlExcelReader reader = new JxlExcelReader(is);
+        reader.setExcelTemplate(et);
+        List<SRoleEntity> beans = reader.readBeans(SRoleEntity.class);
 
         System.out.println("uploadData");
     }
