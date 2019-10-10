c78f86a5598cfa678eafdc53b09aa8d2f0ae64d3
zxhbloomer@gmail.com <Lyyzxh030324>
2019-10-10 22:48:55 +0800

    ··

--- perfect-managerStarter/src/main/java/com/perfect/managerStarter/starter/ManagerClientResourceServerStart.java
+++ perfect-managerStarter/src/main/java/com/perfect/managerStarter/starter/ManagerClientResourceServerStart.java
@@ -1,17 +1,17 @@
 package com.perfect.managerstarter.starter;
 
-import com.perfect.security.properties.PerfectSecurityProperties;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import java.util.List;
+import com.perfect.security.properties.PerfectSecurityProperties;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author zxh
@@ -22,7 +22,7 @@ import java.util.List;
                 "com.perfect.security.*",
                 "com.perfect.redis",
                 "com.perfect.manager.controller",
-        })
+        },exclude = { DataSourceAutoConfiguration.class })
 @EnableTransactionManagement
 @EntityScan(basePackages = {"com.perfect.*"})
 @Slf4j
@@ -34,9 +34,10 @@ public class ManagerClientResourceServerStart {
 
     public static void main(String[] args) {
         log.info("-----------------------启动开始-------------------------");
-        ManagerClientResourceServerStart.config = SpringApplication.run(ManagerClientResourceServerStart.class, args);
-        List<String> packages = AutoConfigurationPackages.get(config);
-        System.out.println("packages: "+packages);
+//        ManagerClientResourceServerStart.config = SpringApplication.run(ManagerClientResourceServerStart.class, args);
+//        List<String> packages = AutoConfigurationPackages.get(config);
+//        System.out.println("packages: "+packages);
+        SpringApplication.run(ManagerClientResourceServerStart.class, args);
         log.info("-----------------------启动完毕-------------------------");
     }
 }
