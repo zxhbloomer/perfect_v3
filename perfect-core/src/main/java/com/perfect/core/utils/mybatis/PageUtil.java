2f6bd39d7b12ae980dcf5f4a65ebd3258932d5b7
zxhbloomer <zxhbloomer@gmail.com>
2019-10-11 16:52:30 +0800

    ..

--- perfect-core/src/main/java/com/perfect/core/utils/mybatis/PageUtil.java
+++ perfect-core/src/main/java/com/perfect/core/utils/mybatis/PageUtil.java
@@ -1,9 +1,6 @@
 package com.perfect.core.utils.mybatis;
 
-import com.baomidou.mybatisplus.core.metadata.OrderItem;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.perfect.bean.config.base.v1.BaseBean;
-import com.perfect.bean.entity.base.entity.v1.BaseEntity;
 import com.perfect.common.utils.string.StringUtil;
 
 /**
@@ -24,35 +21,37 @@ public class PageUtil {
         if (StringUtil.isNotEmpty(sortCondition)) {
             if (sortCondition.startsWith("-")) {
                 // 此为降序
-                page.addOrder(OrderItem.desc(sortCondition.substring(1)));
+                page.setDesc(sortCondition.substring(1));
+//                page.addOrder(OrderItem.desc(sortCondition.substring(1)));
             } else {
                 // 此为升序
-                page.addOrder(OrderItem.asc(sortCondition));
+                page.setAsc(sortCondition);
+//                page.addOrder(OrderItem.asc(sortCondition));
             }
         }
     }
-
-    /**
-     * page排序
-     * @param entityBeanClassName
-     * @param sortCondition
-     * @param <T>
-     * @return
-     * @throws IllegalAccessException
-     * @throws InstantiationException
-     */
-    @Deprecated
-    public static <T> void setSort(Page page , Class<T> entityBeanClassName, String sortCondition)
-        throws IllegalAccessException, InstantiationException {
-        BaseBean<T> be = (BaseBean<T>) entityBeanClassName.newInstance();
-        if (StringUtil.isNotEmpty(sortCondition)) {
-            if (sortCondition.startsWith("-")) {
-                // 此为降序
-                page.addOrder(OrderItem.desc(be.getProperty2Columnn(sortCondition.substring(1))));
-            } else {
-                // 此为升序
-                page.addOrder(OrderItem.asc(be.getProperty2Columnn(sortCondition)));
-            }
-        }
-    }
+//
+//    /**
+//     * page排序
+//     * @param entityBeanClassName
+//     * @param sortCondition
+//     * @param <T>
+//     * @return
+//     * @throws IllegalAccessException
+//     * @throws InstantiationException
+//     */
+//    @Deprecated
+//    public static <T> void setSort(Page page , Class<T> entityBeanClassName, String sortCondition)
+//        throws IllegalAccessException, InstantiationException {
+//        BaseBean<T> be = (BaseBean<T>) entityBeanClassName.newInstance();
+//        if (StringUtil.isNotEmpty(sortCondition)) {
+//            if (sortCondition.startsWith("-")) {
+//                // 此为降序
+//                page.addOrder(OrderItem.desc(be.getProperty2Columnn(sortCondition.substring(1))));
+//            } else {
+//                // 此为升序
+//                page.addOrder(OrderItem.asc(be.getProperty2Columnn(sortCondition)));
+//            }
+//        }
+//    }
 }
